1f6630bf40f0a660aa5e9e397469a3c18557c037
trung vu <trungvu@trungs-MacBook-Pro.local>
2023-02-05 10:52:44 +0700

    edit data

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -5,6 +5,8 @@ const coursesControllers = require('../app/controllers/CoursesControllers')
 
 router.get('/create', coursesControllers.create)
 router.post('/store', coursesControllers.store)
+router.get('/:id/edit', coursesControllers.edit)
+router.put('/:id', coursesControllers.update)
 router.get('/:slug', coursesControllers.show)
 
 module.exports = router
